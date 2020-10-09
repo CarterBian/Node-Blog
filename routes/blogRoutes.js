7f8fa1b852a4f2b173978b3f17039393f0cbd77e
Carter Bian <carterbian32@gmail.com>
2020-10-09 02:12:48 -0700

    require auth before posting

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/', blogController.blog_index);
 router.post('/', blogController.blog_create_post);
-router.get('/create', blogController.blog_create_get);
+router.get('/create', requireAuth, blogController.blog_create_get);
 router.get('/:id', blogController.blog_details)
 router.delete('/:id', blogController.blog_delete);
 
